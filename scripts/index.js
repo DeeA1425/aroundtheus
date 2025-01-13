23232902ae1cfbdc3b13a5d873e8fcf520fb22db
DarnellAccoo <Daccoo25@gmail.com>
2025-01-13 18:00:37 -0500

    inital commit

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,31 @@
-console.log("hello")
+const cardData1 = {
+    title:"Yosemite Valley",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg"
+};
+
+const cardData2 = {
+    title: "Lake Louis",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg"
+};
+
+const cardData3 = {
+    title: "Bald Mountains",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg"
+};
+
+const cardData4 = {
+    title: "Latemar",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg"
+};
+
+const cardData5 = {
+title: "Vanoise National Park",
+link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg"
+};
+
+const cardData6 = {
+title: "Lago di Braies",
+link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg"
+};
+
+console.log(cardData);
