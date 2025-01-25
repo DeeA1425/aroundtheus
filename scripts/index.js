16daaa33481c70ef263cc522b612fd791e011b28
DarnellAccoo <Daccoo25@gmail.com>
2025-01-25 16:45:38 -0500

    inital commit

--- scripts/index.js
+++ scripts/index.js
@@ -1,38 +1,39 @@
 const initialCards = [
-{
-name: "Yosemite Valley",
-link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg"
-},
+  {
+    name: "Yosemite Valley",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
+  },
 
- {
-name: "Lake Louis",
-link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg"
-},
+  {
+    name: "Lake Louis",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg",
+  },
 
-{
-name: "Bald Mountains",
-link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg"
-},
+  {
+    name: "Bald Mountains",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg",
+  },
 
-{
-name: "Latemar",
-link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg"
-},
+  {
+    name: "Latemar",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg",
+  },
 
-{
-name: "Vanoise National Park",
-link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg"
-},
+  {
+    name: "Vanoise National Park",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg",
+  },
 
-{
-name: "Lago di Braies",
-link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg"
-},
+  {
+    name: "Lago di Braies",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
+  },
 ];
 
 const profileEditButton = document.querySelector("#profile_edit-button");
+const profileEditModal = document.querySelector("profile-edit-modal");
 profileEditButton.addEventListener("click", () => {
-console.log("button clicks");
+  console.log("button clicks");
 });
 
 console.log(initialCards);
