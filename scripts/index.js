a675970b7a39909ff4ecc253b6ae6d10d19fd0de
DarnellAccoo <Daccoo25@gmail.com>
2025-02-06 20:00:08 -0500

    inital commit

--- scripts/index.js
+++ scripts/index.js
@@ -44,7 +44,7 @@ const profileDescriptionInput = document.querySelector(
 
 const profileEditForm = profileEditModal.querySelector(".modal__form");
 const cardListEl = document.querySelector(".card__list");
-const cardTemplate = document.querySelector("#.card__template").content.firstElementChild;
+const cardTemplate = document.querySelector("#card-template").content.firstElementChild;
 
 /* -------------------------------------------------------------------------- */
 /*                                  Functions                                 */
