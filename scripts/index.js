f6c4d79065f3d150aba8fade4432fd28eb6f292a
DarnellAccoo <Daccoo25@gmail.com>
2025-03-30 14:37:03 -0400

    inital commit

--- scripts/index.js
+++ scripts/index.js
@@ -210,11 +210,6 @@ function getCardElement(data) {
 
 /* for loop that inserts card */
 
-for (let i = 0; i < initialCards.length; i++) {
-  cardsWrap.prepend(getCardElement(initialCards[i]));
-}
-
 initialCards.forEach((cardData) => {
   cardsWrap.prepend(getCardElement(cardData));
 });
-
