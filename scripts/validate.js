5ea9122b5e86a9e4336c62f6656cb59864dc64a4
DarnellAccoo <Daccoo25@gmail.com>
2025-05-10 17:26:41 -0400

    inital commit

---
+++ scripts/validate.js
@@ -0,0 +1,40 @@
+// enabling validation by calling enableValidation()
+// pass all the settings on call
+
+function setEventListeners(formEl, options) {
+  const { inputSelector } = options;
+  const inputEls = [...formEl.querySelectorAll(inputSelector)];
+
+}
+
+function enableValidation(options) {
+  const formsEls = [...document.querySelectorAll(options.formSelector)];
+  formsEls.forEach((formEl) => {
+    formEl.addEventListener("submit", (e) => {
+      e.preventDefault();
+    });
+
+    setEventListeners(formEl, options);
+    // look for all inputs inside of form
+    // loop through all the inputs to see if all are valid
+    // if input is not valid
+    // get the validation message
+    // add error class to input
+    // display error message
+    // disable button
+    // if all inputs are valid
+    // enable button
+    // reset error messages
+  });
+}
+
+const config = {
+  formSelector: ".modal__form",
+  inputSelector: ".modal__input",
+  submitButtonSelector: ".modal__button",
+  inactiveButtonClass: "modal__button_disabled",
+  inputErrorClass: "modal__input_type_error",
+  errorClass: "modal__error_visible",
+};
+
+enableValidation(config);
